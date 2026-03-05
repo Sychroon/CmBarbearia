61a435d43dc19e0a8e389b31e8b8157b6b3e6ec0
Viktor L.A <jvjoaolima.9@gmail.com>
2026-03-05 00:41:07 -0300

    esqueleto site

---
+++ Program.js
@@ -0,0 +1,8 @@
+
+function abrirModal() {
+    document.querySelector(".Ag-card").style.display = "flex";
+}
+
+function fecharModal() {
+    document.querySelector(".Ag-card").style.display = "none";
+}
